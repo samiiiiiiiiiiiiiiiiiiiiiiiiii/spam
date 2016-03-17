4105d3e05fe042b35dcdbc260b77c4a5250a17e6
samiiiiiiiiiiiiiiiiiiiiiiiiii <samijoni12345678@gmail.com>
2016-03-17 01:04:08 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {85831686},--Sudo users
+    sudo_users = {122997453},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[spammer-bot
